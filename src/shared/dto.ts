c83488493b2639f683b55f8606b0aed4ad7ae759
Diego Torres <diego.torres9799@gmail.com>
2023-08-25 16:05:50 -0700

    Change

--- src/shared/dto.ts
+++ src/shared/dto.ts
@@ -3,12 +3,3 @@ export const productTypes = {
   baked: ['bagels', 'croissant', 'muffins', 'bear claws'],
   drinks: ['coffee', 'tea', 'juice', 'soda', 'energy'],
 } as const
-
-export interface ProductDto {
-  name: string
-  displayName: string
-  price: number
-  type: keyof typeof productTypes
-  families: (typeof productTypes)[keyof typeof productTypes][number][]
-  description: string
-}
