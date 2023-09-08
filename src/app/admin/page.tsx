047953aa59fc1e226924e40229c08ec4312678c2
Diego Torres <diego.torres9799@gmail.com>
2023-09-08 13:56:26 -0700

    update the admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,22 @@
 'use client'
 
 import { PageHeader } from '@/components/page-header/page-header'
+import { Button, Group, Modal } from '@mantine/core'
 import EditProductForm from './edit-product-form'
 
+import { useDisclosure } from '@mantine/hooks'
+
 export default function Admin() {
+  const [opened, { open, close }] = useDisclosure(false)
   return (
     <>
+      <Modal opened={opened} onClose={close}>
+        <EditProductForm />
+      </Modal>
       <PageHeader title="admin" />
-      <EditProductForm />
+      <Group position="center">
+        <Button onClick={open}>Edit/Create Product</Button>
+      </Group>
     </>
   )
 }
